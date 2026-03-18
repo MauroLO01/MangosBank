220fb82870e8a28dec881aff7812ba5a2f24ec34
MauroLO01 <mauriciobentes188@gmail.com>
2026-03-18 15:23:48 -0300

    Co-authored-by: Matheus César  <Matheus0197M@users.noreply.github.com>

---
+++ MangosBank/src/App.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import Home from "./Pages/Home.jsx";
+
+
+function App() {
+  return (
+    <StrictMode>
+      <Home />
+    </StrictMode>
+  );
+}
+
+export default App;
---
+++ MangosBank/src/pages/home.jsx
@@ -0,0 +1,30 @@
+import './home.css';
+
+function Home() {
+  return (
+    <>
+      <section>
+        <header>
+          <nav>
+            <a href="#"></a>
+            <a href="#">Abra sua conta</a>
+            <a href="#">Entrar</a>
+          </nav>
+        </header>
+
+        <div className='main'>
+          <div className='section'>
+            <h1>Plante hoje, colha amanhã</h1>
+            <a href="#">Quero ser MangosBank</a>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        
+      </section>
+    </>
+  );
+}
+
+export default Home;
