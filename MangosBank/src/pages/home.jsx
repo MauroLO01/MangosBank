53453db0e34d5575012099793caaf8de627c2af5
MauroLO01 <mauriciobentes188@gmail.com>
2026-03-20 14:50:14 -0300

    Correção no erro de rotas

--- MangosBank/src/pages/home.jsx
+++ MangosBank/src/pages/home.jsx
@@ -3,21 +3,21 @@ import { Link } from 'react-router-dom';
 import Logo from '../assets/icon-logo2.png';
 import linkPlayStore from '../assets/playstore.png';
 import arrowLinkClient from '../assets/arrow-right2.png';
-import linkAppleStore from '../assets/apple-store.png'; 
+import linkAppleStore from '../assets/apple-store.png';
 
 
 function Home() {
 
   return (
     <>
-      <section>
-        <header>
-          <nav>
-            <Link to="/home">Home</Link>
-            <Link to="/login">Entrar</Link>
-          </nav>
-        </header>
-
+      <div className='body'>
+        <div className='header'>
+          <Link className='headerLink' to="/app"><img className='logo' src={Logo} alt="Home" srcset="" /></Link>
+          <div className='nav'>
+            <Link className='headerLink' to="/cliente">Abra sua conta</Link>
+            <Link className='headerLinkLogin' to="/login">Entrar</Link>
+          </div>
+        </div>
         <div className='main'>
           <div className='section'>
             <img className='logoH1' src={Logo} alt="Logo" />
@@ -30,10 +30,10 @@ function Home() {
         <div className="footer">
           <p className="copy">© 2026 MangosBank. Todos os direitos reservados.</p>
           <div className="linkStores">
-          <p>Baixe o nosso App</p>
+            <p>Baixe o nosso App</p>
             <div className="linkAppStores">
-              <a href="https://play.google.com" target="_blank"><img src={linkPlayStore} alt="Play Store" srcset="" /></a>
-              <a href="https://apps.apple.com" target="_blank"><img src={linkAppleStore} alt="Apple Store" srcset="" /></a>
+              <Link><img src={linkPlayStore} alt="Play Store" srcset="" /></Link>
+              <Link><img src={linkAppleStore} alt="Apple Store" srcset="" /></Link>
             </div>
           </div>
         </div>
