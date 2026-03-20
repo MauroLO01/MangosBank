0d678bb918d32a6d452762291901b6b31d76eb76
MauroLO01 <mauriciobentes188@gmail.com>
2026-03-20 14:56:01 -0300

    Correção no Home

--- MangosBank/src/pages/home.jsx
+++ MangosBank/src/pages/home.jsx
@@ -5,40 +5,49 @@ import linkPlayStore from '../assets/playstore.png';
 import arrowLinkClient from '../assets/arrow-right2.png';
 import linkAppleStore from '../assets/apple-store.png';
 
-
 function Home() {
-
   return (
-    <>
-      <div className='body'>
-        <div className='header'>
-          <Link className='headerLink' to="/app"><img className='logo' src={Logo} alt="Home" srcset="" /></Link>
-          <div className='nav'>
-            <Link className='headerLink' to="/cliente">Abra sua conta</Link>
-            <Link className='headerLinkLogin' to="/login">Entrar</Link>
-          </div>
+    <div className='body'>
+      <div className='header'>
+        <Link className='headerLink' to="/app">
+          <img className='logo' src={Logo} alt="Home" />
+        </Link>
+
+        <div className='nav'>
+          <Link className='headerLink' to="/cliente">Abra sua conta</Link>
+          <Link className='headerLinkLogin' to="/login">Entrar</Link>
         </div>
-        <div className='main'>
-          <div className='section'>
-            <img className='logoH1' src={Logo} alt="Logo" />
-            <h1 className='slogan'>Plante hoje, colha amanhã</h1>
-            <Link className='clienteLink' to="/cliente" target="_blank">
-              Quero ser Mangos <img src={arrowLinkClient} alt="Seta" />
-            </Link>
-          </div>
+      </div>
+
+      <div className='main'>
+        <div className='section'>
+          <img className='logoH1' src={Logo} alt="Logo" />
+          <h1 className='slogan'>Plante hoje, colha amanhã</h1>
+
+          <Link className='clienteLink' to="/cliente">
+            Quero ser Mangos <img src={arrowLinkClient} alt="Seta" />
+          </Link>
         </div>
-        <div className="footer">
-          <p className="copy">© 2026 MangosBank. Todos os direitos reservados.</p>
-          <div className="linkStores">
-            <p>Baixe o nosso App</p>
-            <div className="linkAppStores">
-              <Link><img src={linkPlayStore} alt="Play Store" srcset="" /></Link>
-              <Link><img src={linkAppleStore} alt="Apple Store" srcset="" /></Link>
-            </div>
+      </div>
+
+      <div className="footer">
+        <p className="copy">© 2026 MangosBank. Todos os direitos reservados.</p>
+
+        <div className="linkStores">
+          <p>Baixe o nosso App</p>
+
+          <div className="linkAppStores">
+            <a href="https://play.google.com" target="_blank" rel="noopener noreferrer">
+              <img src={linkPlayStore} alt="Play Store" />
+            </a>
+
+            <a href="https://www.apple.com/app-store/" target="_blank" rel="noopener noreferrer">
+              <img src={linkAppleStore} alt="Apple Store" />
+            </a>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
